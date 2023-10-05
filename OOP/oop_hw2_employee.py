ee2c118c4cdd65c5f3e55f041df664242c4688e4
AlexGnap <oleksandrgnap@gmail.com>
2023-10-05 07:05:15 +0300

    HW 4 part: save_email + validate, exception. TODO: adjust exception and test validation

--- OOP/oop_hw2_employee.py
+++ OOP/oop_hw2_employee.py
@@ -1,13 +1,40 @@
+import csv
+
+
+class EmailAlreadyExistsException(Exception):
+    pass
+
+
 class Employee:
-    def __init__(self, name, daily_salary):
+    def __init__(self, name, daily_salary, email):
         self.name = name
         self.daily_salary = daily_salary
+        self.email = email
+        self.save_email()
 
     def work(self):
         return 'I come to the office.'
 
     def check_salary(self, days):
         return self.daily_salary * days
+
+    def validate(self):
+        # if '@' in self.email:
+        #     return True
+        # else:
+        #     return False
+        with open("email.csv", "r", newline='') as file:
+            reader = csv.reader(file)
+            for i in reader:
+                if i and i[0] == self.email:
+                    raise EmailAlreadyExistsException(self.email)
+
+    def save_email(self):
+        # if self.validate():
+        #     return f'{self.email} is saved'
+        with open("email.csv", "a", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([self.email])
 
     def __lt__(self, other):
         return self.daily_salary < other.daily_salary
@@ -41,10 +68,9 @@ class Recruiter(Employee):
 
 
 class Developer(Employee):
-    def __init__(self, name, daily_salary, tech_stack=None):
-        super().__init__(name, daily_salary)
+    def __init__(self, name, daily_salary, email, tech_stack=None):
+        super().__init__(name, daily_salary, email)
         self.tech_stack = tech_stack or []
-
 
     def get_position(self):
         return 'Developer'
@@ -60,8 +86,6 @@ class Developer(Employee):
         comb_stack = list(set(self.tech_stack + other.tech_stack))
         salary = max(self.daily_salary, other.daily_salary)
         return concat_name, comb_stack, salary
-
-
 
     def __lt__(self, other):
         return len(self.tech_stack) < len(other.tech_stack)
@@ -82,13 +106,13 @@ class Developer(Employee):
         return len(self.tech_stack) >= len(other.tech_stack)
 
 
-a = Employee('John', 25)
+a = Employee('John', 25, 'john@gmail.com')
 print(a.name)
 
-b = Recruiter('Klo', 45)
-c = Developer('New', 48)
-c1 = Developer('Jason', 48, ['Java', 'Ruby'])
-c2 = Developer('Jules', 40, ['Python', 'Java', 'ASP.NET'])
+b = Recruiter('Klo', 45, 'email@ukr.net')
+c = Developer('New', 48, 'new@gmail.com', 'JS')
+c1 = Developer('Jason', 48, 'superdev@gmail.com', ['Java', 'Ruby'])
+c2 = Developer('Jules', 40, 'jb@yahoo.com', ['Python', 'Java', 'ASP.NET'])
 
 print(a.work())
 print(b.work())
@@ -107,3 +131,5 @@ print(c1.check_salary(7))
 
 print(c1 + c2)
 
+print(a.validate())
+print(a.save_email())
