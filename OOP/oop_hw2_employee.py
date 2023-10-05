c9c6bbe90f848ecc4f699e6e82434b84801ed5f0
AlexGnap <oleksandrgnap@gmail.com>
2023-10-05 06:08:37 +0300

    HW 3 part is added: check_salary, tech_stack, new concatenate obj

--- OOP/oop_hw2_employee.py
+++ OOP/oop_hw2_employee.py
@@ -6,6 +6,9 @@ class Employee:
     def work(self):
         return 'I come to the office.'
 
+    def check_salary(self, days):
+        return self.daily_salary * days
+
     def __lt__(self, other):
         return self.daily_salary < other.daily_salary
 
@@ -38,6 +41,10 @@ class Recruiter(Employee):
 
 
 class Developer(Employee):
+    def __init__(self, name, daily_salary, tech_stack=None):
+        super().__init__(name, daily_salary)
+        self.tech_stack = tech_stack or []
+
 
     def get_position(self):
         return 'Developer'
@@ -48,17 +55,55 @@ class Developer(Employee):
     def work(self):
         return 'I come to the office and start to coding..'
 
+    def __add__(self, other):
+        concat_name = self.name + ' ' + other.name
+        comb_stack = list(set(self.tech_stack + other.tech_stack))
+        salary = max(self.daily_salary, other.daily_salary)
+        return concat_name, comb_stack, salary
+
+
+
+    def __lt__(self, other):
+        return len(self.tech_stack) < len(other.tech_stack)
+
+    def __le__(self, other):
+        return len(self.tech_stack) <= len(other.tech_stack)
+
+    def __eq__(self, other):
+        return len(self.tech_stack) == len(other.tech_stack)
+
+    def __ne__(self, other):
+        return len(self.tech_stack) != len(other.tech_stack)
+
+    def __gt__(self, other):
+        return len(self.tech_stack) > len(other.tech_stack)
+
+    def __ge__(self, other):
+        return len(self.tech_stack) >= len(other.tech_stack)
+
 
 a = Employee('John', 25)
 print(a.name)
 
 b = Recruiter('Klo', 45)
 c = Developer('New', 48)
+c1 = Developer('Jason', 48, ['Java', 'Ruby'])
+c2 = Developer('Jules', 40, ['Python', 'Java', 'ASP.NET'])
 
 print(a.work())
 print(b.work())
 print(b)
 print(c)
+print(c1)
 print(b < c)
 print(b <= c)
 print(b == c)
+
+print(c1 < c2)
+
+print(b.check_salary(2))
+print(c.check_salary(4))
+print(c1.check_salary(7))
+
+print(c1 + c2)
+
